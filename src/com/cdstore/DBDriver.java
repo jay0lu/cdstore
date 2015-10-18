fb5c0ac696464ccc04f712dbafd1de32d62a3849
jay0lu <luzheng1992@gmail.com>
2015-10-18 08:36:07 -0400

    add user hibernate

--- src/com/cdstore/DBDriver.java
+++ src/com/cdstore/DBDriver.java
@@ -7,17 +7,22 @@ import java.sql.Statement;
 public class DBDriver {
 	
 	public static Statement connectDb () {
+		try{ 
+		//loading jdbc Driver
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		}catch(Exception ex){
+			System.out.println("Error loading jdbc Driver");
+		}
+
+		//connection to DB
+		String url = "jdbc:mysql://localhost:3306/cdStore"; //DB connection url
 		try{
-			//connection to DB
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cdStore", "root","abc123");
-			
+			Connection myConn = DriverManager.getConnection(url, "root","abc123");
+
 			//create statement
 			Statement myStmt = myConn.createStatement();
-			
-
 			return myStmt;
-		}
-		catch (Exception exc){
+		}catch (Exception exc){
 			exc.printStackTrace();
 		}
 		return null;
